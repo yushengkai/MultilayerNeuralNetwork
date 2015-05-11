76457373467c3620947968b8060f3217861f57f3
YuShengkai <yushengkai@360.cn>
2015-05-11 11:57:38 +0800

    fix bugs

--- torch/nn/load_data.lua
+++ torch/nn/load_data.lua
@@ -2,7 +2,7 @@ require 'torch'
 require 'nn'
 
 trainData = {data=torch.Tensor(9,784), labels=torch.Tensor(9)}
-fid=io.open('../data/unittest.dat', 'r')
+fid=io.open('../../data/unittest.dat', 'r')
 for i=1,9 do
     line=fid:read('*l')
     idx=0
